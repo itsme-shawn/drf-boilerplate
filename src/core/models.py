6c27c4a5b2ab80871f26c88eff9ca947d9c67799
HyeonSoo Choi <chs98105@gmail.com>
2023-02-06 17:55:52 +0900

    [feat]: user 모델 추가 및 테스트 완료
    test_new_user_without_email_raises_error, test_create_superuser

--- src/core/models.py
+++ src/core/models.py
@@ -11,10 +11,21 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Create, save and return a new user"""
+        if not email:
+            raise ValueError("User must have an email address")
         user = self.model(
             email=self.normalize_email(email), **extra_fields
         )  # user 생성할 때 email 필드도 같이 저장
-        user.set_password(password)
+        user.set_password(password)  # password 해시 처리
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Create and return a new superuser"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
         user.save(using=self._db)
 
         return user
--- src/core/tests/test_models.py
+++ src/core/tests/test_models.py
@@ -31,6 +31,20 @@ class ModelTests(TestCase):
         ]
 
         for email, expected in sample_email:
-            user = get_user_model().objects.create_user(email, "123")
+            user = get_user_model().objects.create_user(email=email, password="123")
             # 생성된 user의 email과 expected가 같은지 확인
             self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        """Test that creating user without email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(email=None, password="123")
+
+    def test_create_superuser(self):
+        """Test creating a new superuser"""
+        user = get_user_model().objects.create_superuser(
+            email="test@example.com", password="test123"
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
